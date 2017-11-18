463a5011058b7d141c77b253148e0be84ea97ecc
9886547 <a9886547@9886547s-MacBook-Pro.local>
2017-11-18 07:38:09 +0000

    updated

---
+++ jenkinsOnDocker/jobs/staticLiberty.groovy
@@ -0,0 +1,28 @@
+
+pipelineJob('liberty-docker-cluster') {
+
+  def repo = 'https://github.com/samirtahir91/libertyap.git'
+
+  triggers {
+    scm('H/15 * * * *')
+  }
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master', '**/feature*')
+          scriptPath('Jenkinsfile')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+        // the single line below also works, but it
+        // only covers the 'master' branch and may not give you
+        // enough control.
+        // git(repo, 'master', { node -> node / 'extensions' << '' } )
+      }
+    }
+  }
+}
