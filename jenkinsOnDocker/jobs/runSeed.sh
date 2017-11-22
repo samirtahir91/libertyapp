845ac77e87aeee7fb1ec3839306b0022d8b58682
9886547 <a9886547@9886547s-MacBook-Pro.local>
2017-11-22 10:57:35 +0000

    ranchercompose

--- jenkinsOnDocker/jobs/runSeed.sh
+++ jenkinsOnDocker/jobs/runSeed.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-curl -X POST http://admin:admin@192.168.99.100:8080/job/seedjob/build
+
+jenkins="down"
+
+while [[ ${jenkins} == "down" ]]
+do
+	 if [[ $(curl -X POST http://admin:admin@192.168.99.100:8080/job/seedjob/build) == "" ]]
+		then
+			echo "Seed job executed"
+			jenkins="up"
+	 fi
+done
